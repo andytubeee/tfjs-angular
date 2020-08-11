177c88281ad0523dff9de4d9d8b7da60151c298d
andytubeee <andy.tubeee@gmail.com>
2020-08-11 11:53:23 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  linearModel: tf.Sequential;
+  prediction: any;
   title = 'tfjs-angular';
+
+  ngOnInit() {
+    this.trainNewModel();
+  }
+
+  async trainNewModel() {
+    //Create a model for linear regression
+    this.linearModel = tf.sequential();
+    this.linearModel.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+    
+
+  }
+
 }
